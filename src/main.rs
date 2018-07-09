35ef374c1ddbb5668beae38918a7f3566c42b5c2
Lydia Simmons <saybaar@gmail.com>
2018-07-08 20:28:35 -0700

    Added function to chop strings into list of Note{text,music}

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug)]
+struct Note<'a> {
+    text: &'a str,
+    music: &'a str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let first = env::args().nth(1).expect("Please supply a filename");
+    let mut file = File::open(&first).expect("Error opening file");
+    let mut text = String::new();
+    file.read_to_string(&mut text).expect("Error reading file");
+    println!("Processing string: {}", text);
+    println!("{:?}", text_to_notes(&text.trim()));
+}
+
+//Processes (clumsily) a string like "text(music)text2(music2)..." into a Vec<Note>
+fn text_to_notes<'b>(text: &'b str) -> Vec<Note<'b>> {
+    let splits = text.split(')').filter(|s| s != &"");
+    let mut v: Vec<Note> = Vec::new();
+    for st in splits {
+        let a: Vec<&str> = st.split('(').collect();
+        let n: Note = Note {
+            text: a[0],
+            music: a[1]
+        };
+        v.push(n);
+    }
+    v
 }
