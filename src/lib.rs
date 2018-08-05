77d50e7e605a92db998367172fca5f21708d5043
Lydia Simmons <saybaar@gmail.com>
2018-08-05 16:08:04 -0700

    Clarified struct names, added note prefix scaffolding, pretty-print to string instead of print statement

--- src/lib.rs
+++ src/lib.rs
@@ -23,23 +23,36 @@ pub struct GABCParser;
 
 #[derive(Debug, Serialize)]
 pub struct Note<'a> {
+    pub prefix: &'a str,
+    pub pitch: char,
+    pub suffix: &'a str,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Syllable<'a> {
     pub text: &'a str,
-    pub music: Vec<&'a str>,
+    pub music: Vec<Note<'a>>,
 }
 
 #[derive(Serialize)]
 pub struct GabcFile<'a> {
     pub attributes: Vec<(&'a str, &'a str)>,
-    pub notes: Vec<Note<'a>>,
+    pub syllables: Vec<Syllable<'a>>,
+}
+
+pub fn debug_print(filename: &str) -> String {
+    print_rule_tree(GABCParser::parse(Rule::file, &filename).unwrap(), 0)
 }
 
 ///Pretty-print a parse output tree
-pub fn print_rule_tree(rules: pest::iterators::Pairs<Rule>, tabs: usize) {
+fn print_rule_tree(rules: pest::iterators::Pairs<Rule>, tabs: usize) -> String {
+    let mut output = String::new();
     for rule in rules {
-        for _ in 0..tabs { print!("\t"); }
-        print!("{:?}: {}\n", rule.as_rule(), rule.as_str());
-        print_rule_tree(rule.into_inner(), tabs + 1)
+        for _ in 0..tabs { output.push_str("\t"); }
+        output.push_str(format!("{:?}: {}\n", rule.as_rule(), rule.as_str()).as_ref());
+        output.push_str(print_rule_tree(rule.into_inner(), tabs + 1).as_ref());
     }
+    output
 }
 
 ///Reads a filename and parses it into a GabcFile
@@ -60,8 +73,18 @@ pub fn parse_to_json(filename: &str) -> String {
 }
 
 pub fn parse_to_lilypond(filename: &str) -> String {
-    let notes = "abcdefg";
-    let text = "lorem ipsum dolor sit amet";
+    let full_struct = parse_to_struct(filename);
+    let clef = "c1";
+    let mut notes = String::new();
+    let mut text = String::new();
+    for syllable in &full_struct.syllables {
+        text.push_str(syllable.text);
+        text.push_str(" ");
+        for tone in &syllable.music {
+            notes.push_str(gabc_to_absolute_pitch(tone.pitch, clef));
+            notes.push_str(" ");
+        }
+    }
     format!("{}{}{}{}{}", LY_1, notes, LY_2, text, LY_3)
 }
 
@@ -88,7 +111,7 @@ fn gabc_to_absolute_pitch (gabc_pos: char, clef: &str) -> &str {
 ///Turns a parse result into a GabcFile. This relies on unchecked unwrap() calls that should not
 ///fail because of the characteristics of the pest PEG.
 fn parsed_file_to_struct<'b>(mut parsed_file: pest::iterators::Pairs<'b, Rule>) -> GabcFile<'b> {
-    let mut notes: Vec<Note> = Vec::new();
+    let mut syllables: Vec<Syllable> = Vec::new();
     let mut attributes: Vec<(&str, &str)> = Vec::new();
     for pair in parsed_file.next().unwrap().into_inner() {
         match pair.as_rule() {
@@ -98,13 +121,38 @@ fn parsed_file_to_struct<'b>(mut parsed_file: pest::iterators::Pairs<'b, Rule>) 
                 attributes.push(attribute);
             },
             Rule::syllable => {
-                let strings: Vec<&str> = pair.into_inner().map(|x| x.as_str()).collect();
-                notes.push(Note { text: strings[0], music: strings[1..].to_vec() } );
+                let mut syllable_components = pair.into_inner();
+                let text = syllable_components.next().unwrap().as_str();
+                let mut music: Vec<Note> = Vec::new();
+                while let Some(pair) = syllable_components.next() {
+                    match pair.as_rule() {
+                        Rule::note => {
+                            let mut prefix = "";
+                            let mut pitch = 'z';
+                            let mut suffix = "";
+                            for p in pair.into_inner() {
+                                match &p.as_rule() {
+                                    Rule::prefix => prefix = p.as_str(),
+                                    Rule::position => pitch = p.as_str().chars().next().unwrap(),
+                                    Rule::suffix => suffix = p.as_str(),
+                                    _ => panic!("that's not part of a note!")
+                                }
+                            }
+                            music.push(Note { prefix, pitch, suffix });
+                        },
+                        Rule::clef => {
+
+                        },
+                        _ => unreachable!("impossible syllable sub-rule"),
+                    }
+                }
+                //let strings: Vec<&str> = pair.into_inner().map(|x| x.as_str()).collect();
+                syllables.push(Syllable { text, music } ); //strings[1..].to_vec() } );
             },
             _ => {}
         }
     }
-    GabcFile { attributes, notes }
+    GabcFile { attributes, syllables }
 }
 
 static LY_1: &'static str =
