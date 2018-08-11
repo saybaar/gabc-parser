bc2d8e97b1be2f0f3a262ed7985540344658f97f
Lydia Simmons <saybaar@gmail.com>
2018-08-11 13:21:55 -0700

    Added test files

---
+++ tests/integration-test.rs
@@ -0,0 +1,33 @@
+extern crate gabc_parser;
+use gabc_parser::*;
+use std::fs::File;
+use std::io::Read;
+
+#[test]
+fn test_absolute_pitch() {
+    let note = Note {
+        prefix: "",
+        position: 'd',
+        suffix: "",
+        current_clef: "c1",
+    };
+    assert_eq!("c'", note.absolute_pitch());
+}
+
+#[test]
+fn test_syllable_to_ly() {
+    let syllable = Syllable {
+        text: "*",
+        music: vec![NoteElem::Barline(";")],
+    };
+    assert_eq!("\\divisioMaior", syllable.ly_notes());
+}
+
+#[test]
+fn test_bar() {
+    let mut f = File::open("./tests/bar.gabc").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).expect("error reading bar");
+    let g = GabcFile::new(&s);
+    assert_eq!("test", g.as_lilypond());
+}
